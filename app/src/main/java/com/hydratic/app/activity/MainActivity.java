62ecdf73d48b6e72002fb9ab337cd1786cbf80de
Andreea Militaru <andreea.militaru88@gmail.com>
2019-04-21 10:17:31 -0700

    Add BottomNavigationView

--- app/src/main/java/com/hydratic/app/activity/MainActivity.java
+++ app/src/main/java/com/hydratic/app/activity/MainActivity.java
@@ -1,21 +1,28 @@
 package com.hydratic.app.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.hydratic.app.adapter.MainPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hydratic.app.R;
+import com.hydratic.app.fragment.HistoryFragment;
+import com.hydratic.app.fragment.HydrationFragment;
+import com.hydratic.app.fragment.SettingsFragment;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends FragmentActivity {
 
-    @BindView(R.id.main_view_pager) ViewPager mViewPager;
+    @BindView(R.id.bottom_navigation_bar) BottomNavigationView mBottomNavigationView;
 
-    private PagerAdapter mPagerAdapter;
+    private FragmentManager mFragmentManager;
+    private Fragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,34 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
-        mViewPager.setAdapter(mPagerAdapter);
+        mFragmentManager = getSupportFragmentManager();
+
+        // Set the default fragment
+        mFragmentManager.beginTransaction().replace(
+                R.id.main_fragment_container,
+                new HydrationFragment()).commit();
+
+        mBottomNavigationView.setSelectedItemId(R.id.action_hydration);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch(id) {
+                    case R.id.action_history:
+                        mFragment = new HistoryFragment();
+                        break;
+                    case R.id.action_hydration:
+                        mFragment = new HydrationFragment();
+                        break;
+                    case R.id.action_settings:
+                        mFragment = new SettingsFragment();
+                        break;
+                }
+
+                FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_fragment_container, mFragment).commit();
+                return true;
+            }
+        });
     }
 }
